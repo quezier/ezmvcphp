d715c8dffd9f3af1f13ef8e2f56673f178a34dce
FuYingque <33590896@qq.com>
2017-04-27 15:13:34 +0800

    modify

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 define('IS_DEBUG',true);
 define('IS_REQUEST_FILTER',true);//是否开启对post,get等请求数据的关键字过滤
 define('NOW_TIME',time());
-define('SITE_NAME','世易优品');
+define('SITE_NAME','世易红包系统');
 define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
 define('START_TIME', microtime(true));
 define('START_MEMORY_USAGE', memory_get_usage());
@@ -14,7 +14,7 @@ define('HTTP_DOMAIN', (strtolower($_SERVER['SERVER_PORT']) == 443 ? 'https' : 'h
     . HTTP_HOST . (($p = SERVER_PORT) != 80 AND $p != 443 ? ":$p" : ''));
 define('REQUEST_URI', urldecode($_SERVER['REQUEST_URI']));
 define('PATH', parse_url(REQUEST_URI, PHP_URL_PATH));
-define('IS_SESSION_REDIS',true);//是否使用redis保存session
+define('IS_SESSION_REDIS',false);//是否使用redis保存session
 define('IMG_HOST',HTTP_DOMAIN.'/images/');
 define('JS_HOST',HTTP_DOMAIN);
 define('CSS_HOST',HTTP_DOMAIN);
@@ -23,10 +23,13 @@ define('SESSION_WITH_SUBDOMAIN',false);
 if(SESSION_WITH_SUBDOMAIN)
 {
     ini_set('session.cookie_path', '/');
-    ini_set('session.cookie_domain', '.syypn.test');
+    ini_set('session.cookie_domain', '.hb.test');
     ini_set('session.cookie_lifetime', '0');
 }
-
-
+if(IS_SESSION_REDIS)
+{
+    ini_set('session.save_handler','redis');
+    ini_set('session.save_path','tcp://127.0.0.1:6379');
+}
 require "../vendor/autoload.php";
 require "../boot.php";
